1fc1ca9372ee8cb5a691a0566ff1955128aa1fde
Weyman Fung <wfung@pivotal.io>
2016-09-23 13:49:12 -0700

    WIP: add svg to styleguide
    Signed-off-by: Jonathan Berney <jberney@pivotal.io>

---
+++ styleguide/docs/react/svg.js
@@ -0,0 +1,34 @@
+/*doc
+ ---
+ title: Svg
+ name: svg
+ categories:
+ - react_components_svg
+ - react_all
+ ---
+
+ <code class="pam">
+ <i class="fa fa-download" alt="Install the Component">
+ npm install pui-react-svg  --save
+ npm install svg-react-loader  --save-dev
+ </i>
+ </code>
+
+ Require the subcomponents:
+
+ ```
+var Svg = require('pui-react-svg').Svg;
+ ```
+
+
+ ```react_example
+<Svg src="check" width="20" height="20" />
+ ```
+
+ The Svg Components require the following property:
+
+ Property           |  Type         | Description
+ -------------      | --------------| --------------------------------------------------------------------------
+ `src`       | String        | Name of the svg (excluding the .svg extension)
+
+ */
